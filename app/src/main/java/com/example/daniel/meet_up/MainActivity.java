8f00b3bbaa0bc67b8b8707298b7014b39dd4bda0
Daniel Miller <miller.6227@osu.edu>
2017-02-22 13:39:06 -0500

    recommit before creating fragements for other pages

--- app/src/main/java/com/example/daniel/meet_up/MainActivity.java
+++ app/src/main/java/com/example/daniel/meet_up/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.daniel.meet_up;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
